fb963ead64d13f8d23567a5acc80f9e34adf75a6
pabloamaral <pabz.amaral@gmail.com>
2026-04-15 12:54:19 -0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ load_dotenv(dotenv_path=Path(__file__).parent.parent / ".env")
 
 GLEAN_INSTANCE = os.environ["GLEAN_INSTANCE"]
 CLIENT_TOKEN = os.environ["GLEAN_CLIENT_TOKEN"]
-DATASOURCE = os.environ["GLEAN_DATASOURCE"]
+DATASOURCE = "interviewds"
 
 ACT_AS_EMAIL = os.environ.get("GLEAN_ACT_AS_EMAIL", "alex@glean-sandbox.com")
 
